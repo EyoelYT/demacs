4fc21033ec71733b70c5fa49c0c727ff1392b14e
Eyoel Tesfu <EyoelYTesfu@gmail.com>
2025-08-04 21:26:00 -0500

    Override `eval-expression` and `pp-eval-expression`
    Add opportunity to insert the current active region into the eval
    prompt.

--- functions.el
+++ functions.el
@@ -1618,6 +1618,79 @@ the beginning of the line."
 ;;     (apply fn args)))
 ;; (advice-add #'repeat-mode :around #'shut-up--advice)
 
+(defun eval-expression (exp &optional insert-value no-truncate char-print-limit)
+  "Evaluate EXP and print value in the echo area.
+When called interactively, read an Emacs Lisp expression and
+evaluate it.  Value is also consed on to front of the variable
+`values'.  Optional argument INSERT-VALUE non-nil (interactively,
+with a non `-' prefix argument) means insert the result into the
+current buffer instead of printing it in the echo area.
+
+Normally, this function truncates long output according to the
+value of the variables `eval-expression-print-length' and
+`eval-expression-print-level'.  When NO-TRUNCATE is
+non-nil (interactively, with a prefix argument of zero), however,
+there is no such truncation.
+
+If the resulting value is an integer, and CHAR-PRINT-LIMIT is
+non-nil (interactively, unless given a non-zero prefix argument)
+it will be printed in several additional formats (octal,
+hexadecimal, and character).  The character format is used only
+if the value is below CHAR-PRINT-LIMIT (interactively, if the
+prefix argument is -1 or the value doesn't exceed
+`eval-expression-print-maximum-character').
+
+Runs the hook `eval-expression-minibuffer-setup-hook' on entering the
+minibuffer.
+
+If `eval-expression-debug-on-error' is non-nil, which is the default,
+this command arranges for all errors to enter the debugger."
+  (interactive
+   (let ((selection
+          (if (ey/region-active-p)
+              (buffer-substring-no-properties (ey/region-beginning) (ey/region-end))
+            nil)))
+   (cons (read--expression "Eval: " selection)
+         (eval-expression-get-print-arguments current-prefix-arg))))
+
+  (let* (result
+         (runfun
+          (lambda ()
+            (setq result
+                  (values--store-value
+                   (eval (let ((lexical-binding t)) (macroexpand-all exp))
+                         t))))))
+    (if (null eval-expression-debug-on-error)
+        (funcall runfun)
+      (handler-bind ((error #'eval-expression--debug))
+        (funcall runfun)))
+
+    (let ((print-length (unless no-truncate eval-expression-print-length))
+          (print-level  (unless no-truncate eval-expression-print-level))
+          (eval-expression-print-maximum-character char-print-limit)
+          (deactivate-mark))
+      (let ((out (if insert-value (current-buffer) t)))
+        (prog1
+            (prin1 result out)
+          (let ((str (and char-print-limit
+                          (eval-expression-print-format result))))
+            (when str (princ str out))))))))
+
+(after! pp
+  (defun pp-eval-expression (expression)
+  "Evaluate EXPRESSION and pretty-print its value.
+Also add the value to the front of the list in the variable `values'."
+  (interactive
+   (let ((selection
+          (if (ey/region-active-p)
+              (buffer-substring-no-properties (ey/region-beginning) (ey/region-end))
+            nil)))
+     (list (read--expression "Eval: " selection))))
+  (message "Evaluating...")
+  (let ((result (eval expression lexical-binding)))
+    (values--store-value result)
+    (pp-display-expression result "*Pp Eval Output*" pp-use-max-width))))
+
 
 
 ;;; Focused Window Automatic Resize; Ref: https://www.reddit.com/r/emacs/comments/1mf3cmk/autoresizing_on_focused_windows
