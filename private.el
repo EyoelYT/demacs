b1413a999608ca10ceefdb5b58ef2b44e24b0778
Eyoel Tesfu <EyoelYTesfu@gmail.com>
2025-08-29 21:40:56 -0500

    Clues to my privates

--- private.el
+++ private.el
@@ -6,3 +6,18 @@
 ;; `org-journal-dir',
 ;; `projectile-project-search-path'
 ;; `find-function-C-source-directory'
+
+(after! org
+  (setq org-directory nil)
+  (setq org-id-locations-file nil)
+  (setq org-agenda-files nil)
+(after! org-roam
+  (setq org-roam-directory nil))
+
+(after! org-journal
+  (setq org-journal-dir nil))
+
+(after! projectile
+  (setq projectile-project-search-path nil)
+
+(setq find-function-C-source-directory nil)
