0c7e4d4db8536b8e7ff79be9cd9f0ac6783c82cf
Eyoel Tesfu <eyoeltesfu@Mac.lan>
2025-10-25 16:06:32 -0500

    Reorder file loads

--- config.el
+++ config.el
@@ -2,8 +2,8 @@
 
 
 
-(load (concat doom-user-dir "defaults.el"))
-(load (concat doom-user-dir "packagings.el"))
 (load (concat doom-user-dir "functions.el"))
+(load (concat doom-user-dir "packagings.el"))
+(load (concat doom-user-dir "defaults.el"))
 (load (concat doom-user-dir "mappings.el"))
 (load (concat doom-user-dir "private.el"))
