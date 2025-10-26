94d8064cc63f38ac507222600b24c32d1a6761d9
EyoelYT <eyoelytesfu@gmail.com>
2025-10-26 01:15:29 -0500

    Fix errors

--- private.el
+++ private.el
@@ -15,18 +15,3 @@
 ;;   (setq projectile-project-search-path nil))
 ;;
 ;; (setq find-function-C-source-directory nil)
-(after! org
-  (setq org-directory nil)
-  (setq org-id-locations-file nil)
-  (setq org-agenda-files nil)
-  (setq +org-capture-todo-file nil))
-(after! org-roam
-  (setq org-roam-directory nil))
-
-(after! org-journal
-  (setq org-journal-dir nil))
-
-(after! projectile
-  (setq projectile-project-search-path nil))
-
-(setq find-function-C-source-directory nil)
